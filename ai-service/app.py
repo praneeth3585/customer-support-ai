8c23efdd213c0f85f0b03e6c4077b5c9d4c132f9
praneeth3585 <praneethchowdary35@gmail.com>
2026-06-02 17:55:02 +0530

    Restrict CORS to Vercel

--- ai-service/app.py
+++ ai-service/app.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=[https://customer-support-ai-green-zeta.vercel.app/]
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
